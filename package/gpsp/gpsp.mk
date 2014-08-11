01bdb23854e6fff683413b1d249e0de2f6d79b1d
MrMonotone <nicholas95@comcast.net>
2014-08-11 10:19:51 +0000

    got gpsp to start to compile cant find SDL

--- package/gpsp/gpsp.mk
+++ package/gpsp/gpsp.mk
@@ -5,13 +5,12 @@
 ################################################################################
 GPSP_VERSION = d884dfcc1bf6951473b28cda5d34cceec093a263
 GPSP_SITE = $(call github,gizmo98,gpsp,$(GPSP_VERSION))
-#RETROARCH_LICENSE = GPLv3+
-#RETROARCH_CONF_OPT += --disable-netplay --disable-oss 
-#--disable-bsv_movie
-#RETROARCH_DEPENDENCIES = host-pkgconf
+GPSP_DEPENDENCIES = sdl
 
 define GPSP_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/raspberrypi all
+	$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		-C $(@D)/raspberrypi all
 endef
 
 define GPSP_INSTALL_TARGET_CMDS
