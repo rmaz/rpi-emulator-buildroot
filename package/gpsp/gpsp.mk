c2dc75242237da9b267203e44c74efe58ecedd5f
Nicholas <Nicholas95@comcast.net>
2014-08-12 20:45:17 -0700

    Update gpsp.mk
    used correct directories

--- package/gpsp/gpsp.mk
+++ package/gpsp/gpsp.mk
@@ -6,8 +6,8 @@
 GPSP_VERSION = d884dfcc1bf6951473b28cda5d34cceec093a263
 GPSP_SITE = $(call github,gizmo98,gpsp,$(GPSP_VERSION))
 GPSP_DEPENDENCIES = sdl
-GPSP_CFLAGS = -DARM_ARCH -DRPI_BUILD -Wall `sdl-config --cflags` -Ofast -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -fgnu89-inline -I${includedir} -I${includedir}/interface/vcos/pthreads -I${includedir}/interface/vmcs_host/linux
-GPSP_LIBS = `sdl-config --libs` -ldl -lpthread -L${libdir} -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
+GPSP_CFLAGS = -DARM_ARCH -DRPI_BUILD -Wall `sdl-config --cflags` -Ofast -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -fgnu89-inline -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
+GPSP_LIBS = `sdl-config --libs` -ldl -lpthread -L$(STAGING_DIR)/usr/lib -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
 
 define GPSP_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" \
