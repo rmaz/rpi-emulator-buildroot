f1e00c5acdaa730c365a050e3a798f2bd0724f54
MrMonotone <nicholas95@comcast.net>
2014-08-13 22:38:58 +0000

    readded missing libs I think?

--- package/gpsp/gpsp.mk
+++ package/gpsp/gpsp.mk
@@ -9,8 +9,8 @@ GPSP_DEPENDENCIES = sdl
 SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config
 SDL_FLAGS=`$(SDL_CONFIG) --cflags`
 SDL_LIBS=`$(SDL_CONFIG) --libs`
-GPSP_CFLAGS=$(SDL_FLAGS) -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
-GPSP_LIBS=$(SDL_LIBS) -ldl -lpthread -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
+GPSP_CFLAGS= -DARM_ARCH -DRPI_BUILD -Wall -Ofast -mfpu=vfp -mfloat-abi=hard -march=armv6j -fgnu89-inline $(SDL_FLAGS) -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
+GPSP_LIBS= -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib $(SDL_LIBS) -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
 
 define GPSP_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" \
@@ -20,7 +20,8 @@ define GPSP_BUILD_CMDS
 endef
 
 define GPSP_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+	$(INSTALL) -D $(@D)/gpsp \
+		$(TARGET_DIR)/usr/emulators/gpsp
 endef
 
 $(eval $(generic-package))
