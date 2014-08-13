1c3a3e17b621314c58ccb10c566f3b8197ee1988
MrMonotone <nicholas95@comcast.net>
2014-08-13 07:35:24 +0000

    cant read from rpi.h now??

--- package/gpsp/gpsp.mk
+++ package/gpsp/gpsp.mk
@@ -6,13 +6,17 @@
 GPSP_VERSION = d884dfcc1bf6951473b28cda5d34cceec093a263
 GPSP_SITE = $(call github,gizmo98,gpsp,$(GPSP_VERSION))
 GPSP_DEPENDENCIES = sdl
-GPSP_CFLAGS = -DARM_ARCH -DRPI_BUILD -Wall `sdl-config --cflags` -Ofast -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -fgnu89-inline -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
-GPSP_LIBS = `sdl-config --libs` -ldl -lpthread -L$(STAGING_DIR)/usr/lib -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
+SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config
+SDL_FLAGS=`$(SDL_CONFIG) --cflags`
+SDL_LIBS=`$(SDL_CONFIG) --libs`
+GPSP_CFLAGS=$(SDL_FLAGS) -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
+GPSP_LIBS=$(SDL_LIBS) -ldl -lpthread -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm  -lrt
 
 define GPSP_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS) $(GPSP_CFLAGS)" \
-		LIBS="$(GPSP_LIBS)" -C $(@D)/raspberrypi all
+		LIBS="$(GPSP_LIBS)" \
+		-C $(@D)/raspberrypi all
 endef
 
 define GPSP_INSTALL_TARGET_CMDS
